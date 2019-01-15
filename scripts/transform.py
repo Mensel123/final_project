4258e4f8c01e2741a54b8c1ad5d18fd41cc40181
Mendel Engelaer <mendelengelaer@wcw-stud-145-109-61-137.wireless.uva.nl>
2019-01-15 16:04:44 +0100

    drie grafieken af

--- scripts/transform.py
+++ scripts/transform.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import csv
+
 import json
 from pandas.io.json import json_normalize
 
@@ -34,20 +35,64 @@ def load():
     # with pd.option_context('display.max_columns', None):
     #     print(result)
     # result.set_index("Country Name", inplace=True)
+    # result.fillna(, inplace=True)
+    result = result.where((pd.notnull(result)), None)
+    print(result)
 
 
-
-    result.to_json("new_json.json", orient='records')
+    # print(result)
+    result = result.to_dict(orient='records') #"new_json.json",
+    # result[0]['2016']= {'2017':result[0]['2015'],'2018':result[0]['2015']}
+    # print(result)
 
     # columns = result.columns.tolist()
     # columns.remove('coordinates')
     # columns.remove('type')
     # print(result.coordinates)
 
-    # temp_list = []
-    # for year in range(1990,2016):
-    #    jaar = str(year)
-    #    temp_list.append(jaar)
+    temp_list = []
+    for year in range(1990,2016):
+       jaar = str(year)
+       temp_list.append(jaar)
+    # print(temp_list)
+    # for i in range(len(result)):
+    #     result[i]['Years'] = {"year":'1990', "value":result[i]['1990']},{"year":'1991',"value":result[i]['1991']},{"year":'1992',"value":result[i]['1992']},
+    #     {"year":'1993',"value":result[i]['1993']},{"year":'1994',"value":result[i]['1994']},{"year":'1995',"value":result[i]['1995']},{"year":'1996',"value":result[i]['1996']},
+    #     {"year":'1997',"value":result[i]['1997']},{"year":'1998',"value":result[i]['1998']},{"year":'1999',"value":result[i]['1999']},{"year":'2000',"value":result[i]['2000']},
+    #     {"year":'2001',"value":result[i]['2001']},{"year":'2002',"value":result[i]['2002']},{"year":'2003',"value":result[i]['2003']},{"year":'2004',"value":result[i]['2004']},
+    #     {"year":'2005',"value":result[i]['2005']},{"year":'2006',"value":result[i]['2006']},{"year":'2007',"value":result[i]['2007']},{"year":'2008',"value":result[i]['2008']},
+    #     {"year":'2009',"value":result[i]['2009']},{"year":'2010',"value":result[i]['2010']},{"year":'2011',"value":result[i]['2011']},{"year":'2012',"value":result[i]['2012']},
+    #     {"year":'2013',"value":result[i]['2013']},{"year":'2014',"value":result[i]['2014']},{"year":'2015',"value":result[i]['2015']}
+    #     for year in temp_list:
+    #         del(result[i][year])
+    temp_dict = dict()
+    temp_list_2 = []
+    # for i in range(len(result)):
+    #     for year in temp_list:
+    #         temp_list_2 = (result[i][year])
+    # print(dict(zip(temp_list, temp_list_2))
+        # dict(zip(temp_list, values))
+    for i in range(len(result)):
+        temp_list_2 = []
+        for year in temp_list:
+            temp_list_2.append({"year":year, "value":result[i][year]})
+        result[i]['Years'] = temp_list_2
+        for year in temp_list:
+            del(result[i][year])
+    print(result)
+    # print(result[0])
+    # for i in result:
+    #     for j in i:
+    #
+    #         if i[j] == 'NaN':
+    #             i[j] == 'null'
+    #         print(i[j])
+
+    with open('new_json.json', 'w') as outfile:
+        json.dump(result, outfile)
+
+
+
     # print(temp_list)
     # for datum in temp_list:
     #     columns.remove(datum)
